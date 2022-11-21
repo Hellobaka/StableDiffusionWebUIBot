f0400f620cbcde1f256710dcbac221356415f7d8
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-11-21 10:36:16 +0800

    完成img2img逻辑

--- me.cqp.luohuaming.NovelAI.Code/OrderFunctions/Img2img.cs
+++ me.cqp.luohuaming.NovelAI.Code/OrderFunctions/Img2img.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using me.cqp.luohuaming.NovelAI.Sdk.Cqp;
 using me.cqp.luohuaming.NovelAI.Sdk.Cqp.EventArgs;
+using me.cqp.luohuaming.NovelAI.Sdk.Cqp.Model;
 using PublicInfos;
+using PublicInfos.API;
 using PublicInfos.Config;
 
 namespace me.cqp.luohuaming.NovelAI.Code.OrderFunctions
@@ -12,30 +17,155 @@ namespace me.cqp.luohuaming.NovelAI.Code.OrderFunctions
     public class Img2Img : IOrderModel
     {
         public bool ImplementFlag { get; set; } = true;
-        
+
+        public Dictionary<(long, long), string> GroupImageList = new();
+
         public string GetOrderStr() => OrderConfig.Img2Img;
 
-        public bool Judge(string destStr) => destStr.Replace("＃", "#").StartsWith(GetOrderStr());
+        public bool Judge(string destStr) => true;
 
-        public FunctionResult Progress(CQGroupMessageEventArgs e)//群聊处理
+        public FunctionResult Progress(CQGroupMessageEventArgs e)
         {
             FunctionResult result = new FunctionResult
             {
                 Result = true,
                 SendFlag = true,
             };
+
+            string imgBase64 = "";
+            if (GroupImageList.Any(x => x.Key == (e.FromGroup, e.FromQQ)))
+            {
+                if (e.Message.IsImageMessage is false)
+                {
+                    result.SendFlag = false;
+                    result.Result = false;
+                    return result;
+                }
+                else
+                {
+                    string img = e.CQApi.ReceiveImage(e.Message.Text);
+                    imgBase64 = Convert.ToBase64String(File.ReadAllBytes(img));
+                    // GroupImageList.Remove((e.FromGroup, e.FromQQ));
+                }
+            }
+            else
+            {
+                if (e.Message.Text.Replace("＃", "#").StartsWith(GetOrderStr()) is false)
+                {
+                    result.SendFlag = false;
+                    result.Result = false;
+                    return result;
+                }
+            }
+
             SendText sendText = new SendText
             {
                 SendID = e.FromGroup,
             };
             result.SendObject.Add(sendText);
-            if (AppConfig.Using)
+            if (AppConfig.R18PunishList.Any(x => x == e.FromQQ))
             {
-                sendText.MsgToSend.Add("当前有任务正在进行，请等待前一任务完成...");
+                result.SendFlag = false;
                 return result;
             }
 
-            sendText.MsgToSend.Add("这里输入需要发送的文本");
+            if (AppConfig.Using)
+            {
+                sendText.MsgToSend.Add(AppConfig.BusyResponse);
+                return result;
+            }
+
+            if(imgBase64 == "")
+            {
+                if (QuotaHistory.QueryQuota(e.FromGroup, e.FromQQ) <= 0)
+                {
+                    sendText.MsgToSend.Add(AppConfig.NoQuotaResponse);
+                    return result;
+                }
+                else
+                {
+                    int quota = AppConfig.MaxPersonQuota - QuotaHistory.HandleQuota(e.FromGroup, e.FromQQ, -1);
+                    e.FromGroup.SendGroupMessage(AppConfig.CallResponse.Replace("\\n", "\n").Replace("%count%", quota.ToString()));
+                }
+            }
+
+            try
+            {
+                string prompt = e.Message.Text.Replace(GetOrderStr(), "").Replace("，", ",");
+                if (AppConfig.UseTranslate && !string.IsNullOrEmpty(prompt))
+                {
+                    string translateResult = Translate.CallTranslate(prompt);
+                    if (translateResult == "err")
+                    {
+                        // sendText.MsgToSend.Add("翻译API无效...");
+                        MainSave.CQLog.Info("翻译API无效", "请打开配置文件填写相关字段");
+                    }
+                    else
+                    {
+                        prompt = translateResult;
+                    }
+                }
+
+                if(imgBase64 == "")// 
+                {
+                    List<CQCode> cqList = CQCode.Parse(e.Message.Text);
+                    var imgCQCode = cqList.FirstOrDefault(x => x.IsImageCQCode);
+                    if (imgCQCode == null)
+                    {
+                        GroupImageList.Add((e.FromGroup, e.FromQQ), prompt);
+                        sendText.MsgToSend.Add("请在接下来的消息内发送一张图片");
+                        return result;
+                    }
+                    else
+                    {
+                        imgBase64 = Convert.ToBase64String(File.ReadAllBytes(e.CQApi.ReceiveImage(imgCQCode)));
+                    }
+                }
+
+                var r = PublicInfos.API.NovelAI.Img2Img(imgBase64, AppConfig.BasePrompt + prompt);
+
+                if (r.IsSuccess)
+                {
+                    string img = r.Result;
+                    string filename = DateTime.Now.ToString("G").Replace("/", "").Replace(":", "").Replace(" ", "");
+                    string path = Path.Combine(MainSave.ImageDirectory, "NovelAI");
+                    Directory.CreateDirectory(path);
+                    path = Path.Combine(path, filename + ".png");
+                    File.WriteAllBytes(path, Convert.FromBase64String(img));
+                    if (AppConfig.R18 == false && r.R18)
+                    {
+                        MainSave.CQLog.Info("绘制失败", "R18触发");
+                        sendText.MsgToSend.Add(AppConfig.R18PunishResponse.Replace("%time%", AppConfig.R18PunishTime.ToString()));
+                        AppConfig.R18PunishList.Add(e.FromQQ);
+                        new Thread(() =>
+                        {
+                            Thread.Sleep(AppConfig.R18PunishTime * 1000);
+                            AppConfig.R18PunishList.Remove(e.FromQQ);
+                        }).Start();
+                        return result;
+                    }
+                    int msgId = e.FromGroup.SendGroupMessage(CQApi.CQCode_Image($"NovelAI\\{filename}.png").ToSendString()).Id;
+
+                    if (AppConfig.R18)
+                    {
+                        if (r.R18)
+                        {
+                            // 自动撤回(暂时无法判断图片是否为R18
+                        }
+                    }
+                }
+                else
+                {
+                    QuotaHistory.HandleQuota(e.FromGroup, e.FromQQ, 1);
+                    sendText.MsgToSend.Add("绘制失败...");
+                }
+            }
+            catch (Exception exc)
+            {
+                QuotaHistory.HandleQuota(e.FromGroup, e.FromQQ, 1);
+                sendText.MsgToSend.Add($"绘制失败..., {exc.Message}");
+                MainSave.CQLog.Info("绘制失败", exc.Message + exc.StackTrace);
+            }
             return result;
         }
 
--- me.cqp.luohuaming.NovelAI.PublicInfos/API/NovelAI.cs
+++ me.cqp.luohuaming.NovelAI.PublicInfos/API/NovelAI.cs
@@ -13,6 +13,12 @@ public class NovelAI
             , AppConfig.RestoreFaces, AppConfig.SamplingMethod);
     }
 
+    public static APIResult Img2Img(string img, string prompt)
+    {
+        return Img2Img(img, prompt, AppConfig.NegativePrompt, AppConfig.Steps, AppConfig.Height, AppConfig.Width
+            , AppConfig.RestoreFaces, AppConfig.SamplingMethod);
+    }
+
     public static APIResult Txt2Img(string prompt, string negative_prompt, int steps, int height, int width,
         bool restore_faces, string engine)
     {
@@ -76,38 +82,14 @@ public class NovelAI
   ],
   ""resize_mode"": 0,
   ""denoising_strength"": 0.75,
-  ""mask"": ""string"",
-  ""mask_blur"": 4,
-  ""inpainting_fill"": 0,
-  ""inpaint_full_res"": true,
-  ""inpaint_full_res_padding"": 0,
-  ""inpainting_mask_invert"": 0,
   ""prompt"": ""%prompt%"",
-  ""styles"": [
-    """"
-  ],
-  ""seed"": -1,
-  ""subseed"": -1,
-  ""subseed_strength"": 0,
-  ""seed_resize_from_h"": -1,
-  ""seed_resize_from_w"": -1,
-  ""batch_size"": 1,
-  ""n_iter"": 1,
   ""steps"": %steps%,
   ""cfg_scale"": 7,
   ""width"": %width%,
   ""height"": %height%,
   ""restore_faces"": %restore_faces%,
-  ""tiling"": false,
   ""negative_prompt"": ""%negative_prompt%"",
-  ""eta"": 0,
-  ""s_churn"": 0,
-  ""s_tmax"": 0,
-  ""s_tmin"": 0,
-  ""s_noise"": 1,
-  ""override_settings"": {},
-  ""sampler_index"": ""%engine%"",
-  ""include_init_images"": false
+  ""sampler_index"": ""%engine%""
 }";
             body = body.Replace("%prompt%", prompt)
                 .Replace("%negative_prompt%", negative_prompt)
